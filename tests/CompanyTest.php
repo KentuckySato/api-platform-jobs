57ba0fd60a7632c35cecfd57dd356955cde706a4
Kentucky SATO <kentucky.sato@gmail.com>
2023-06-02 15:20:26 +0200

    test: create Company Test ok

--- tests/CompanyTest.php
+++ tests/CompanyTest.php
@@ -11,10 +11,12 @@ class CompanyTest extends ApiTestCase
     // This trait provided by AliceBundle will take care of refreshing the database content to a known state before each test
     use RefreshDatabaseTrait;
 
+    const URL = '/api/companies';
+
     public function testGetCollection(): void
     {
         // The client implements Symfony HttpClient's `HttpClientInterface`, and the response `ResponseInterface`
-        $response = static::createClient()->request('GET', '/api/companies');
+        $response = static::createClient()->request('GET', self::URL);
 
         $this->assertResponseIsSuccessful();
         // Asserts that the returned content type is JSON-LD (the default)
@@ -35,5 +37,33 @@ class CompanyTest extends ApiTestCase
         // Asserts that the returned JSON is validated by the JSON Schema generated for this resource by API Platform
         // This generated JSON Schema is also used in the OpenAPI spec!
         $this->assertMatchesResourceCollectionJsonSchema(Company::class);
+    }
+
+    public function testCreateCompany(): void
+    {
+        $response = static::createClient()->request('POST', self::URL, ['json' => [
+            "name" => "Facebook Test Company",
+            "description" => "My Company Test",
+            "phone" => "+33987988754",
+            "website" => "https://facebook.fr",
+            "siren" => "123",
+            "siret" => "456"
+        ]]);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Company',
+            '@type' => 'Company',
+            "name" => "Facebook Test Company",
+            "description" => "My Company Test",
+            "phone" => "+33987988754",
+            "website" => "https://facebook.fr",
+            "siren" => "123",
+            "siret" => "456",
+            "recruiters" => []
+        ]);
+        $this->assertMatchesRegularExpression('~^/api/companies/\d+$~', $response->toArray()['@id']);
+        $this->assertMatchesResourceItemJsonSchema(Company::class);
     }
 }
